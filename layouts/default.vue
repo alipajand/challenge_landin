bcaaac1273ce3af75161b8a0fe769a04a96c68b9
alipajand <ali.pajand@gmail.com>
2019-12-25 08:02:05 +0330

    :lipstick: Update main bg

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="global-container">
-    <transition
-      enter-active-class="page-enter-active"
-      leave-active-class="page-leave-active"
-      enter-to-class="page-enter"
-      leave-to-class="page-leave-to"
-    >
-      <nuxt :key="$route.fullPath" class="transitioning" />
-    </transition>
-  </div>
+  <transition
+    enter-active-class="page-enter-active"
+    leave-active-class="page-leave-active"
+    enter-to-class="page-enter"
+    leave-to-class="page-leave-to"
+  >
+    <nuxt :key="$route.fullPath" class="transitioning" />
+  </transition>
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,26 @@
 <template>
-  <div class="v-center rtl">
-    <b-container>
-      <b-row class="justify-content-center">
-        <b-col lg="9" cols="12">
-          <b-card class="border-0 rounded-expanded position-relative index-question-wrapper">
-            <b-card class="border-0 index-top-header shadow-large text-center">
-              چقدر از کیفیت خدمات ما راضی هستید؟
+  <div>
+    <div class="index-main-bg position-absolute h-100 w-100" />
+    <div class="index-main-wave-container position-absolute h-100 w-100 overflow-hidden">
+      <div class="index-main-wave left-top position-absolute" />
+      <div class="index-main-wave right-bottom position-absolute" />
+    </div>
+    <div class="v-center rtl">
+      <b-container>
+        <b-row class="justify-content-center">
+          <b-col lg="9" cols="12">
+            <b-card class="border-0 rounded-expanded position-relative index-question-wrapper">
+              <b-card class="border-0 index-top-header shadow-large text-center">
+                چقدر از کیفیت خدمات ما راضی هستید؟
+              </b-card>
+              <div class="p-4">
+                سلام
+              </div>
             </b-card>
-            <div class="p-4">
-              سلام
-            </div>
-          </b-card>
-        </b-col>
-      </b-row>
-    </b-container>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
   </div>
 </template>
 
@@ -23,6 +30,34 @@
 
 <style scoped lang="scss">
   @import "../assets/scss/dep";
+
+  .index-main-bg {
+    z-index: -2;
+    background: url('/img/bg.png') center center no-repeat;
+    background-size: cover;
+  }
+
+  .index-main-wave-container {
+    .index-main-wave {
+      z-index: -1;
+      width: 50%;
+      height: 30%;
+      background: url('/img/waves.png') center center no-repeat;
+      background-size: contain;
+
+      &.left-top {
+        top: -4%;
+        left: -10%;
+        @include transform(rotate(180deg));
+      }
+
+      &.right-bottom {
+        right: -10%;
+        bottom: -4%;
+        @include opacity(0.7)
+      }
+    }
+  }
 
   .index-question-wrapper {
     &.rounded-expanded {
